7fca4af57440f8cda8f4285899c3455453c115eb
BryantCR <bryancvargas20@gmail.com>
2021-09-23 14:52:25 -0600

    all working good

--- server.py
+++ server.py
@@ -21,16 +21,51 @@ def hiMichael():
 @app.route('/say/jonh')
 def hiJonh():
     return 'Hi Jonh!'
+    
+@app.route('/say/<name>', methods = ['GET'])
+def hiName( name ):
+    return 'Hi '+ name
 
 #------------------------------PART III
-@app.route('/repeat/35/hello')
-def sayHello():
-    hello = ""
-    for i in range (1, 36, 1)
-        print("hello")
-    return 'Hi Flask!'
+# @app.route('/repeat/35/hello')
+# def sayHello():
+#     hello = ""
+#     for i in range (1, 36, 1):
+#         hello = hello +
+#     return hello
+
+# @app.route('/repeat/80/bye')
+# def sayBye():
+#     hello = ""
+#     for i in range (1, 81, 1):
+#         print("bye")
+#     return 'Hi Flask!'
+
+# @app.route('/repeat/99/dog')
+# def sayDog():
+#     hello = ""
+#     for i in range (1, 100, 1):
+#         print("dog")
+#     return 'Hi Flask!'
+
+@app.route('/repeat/<num>/<word>')
+def repeatNumWord(num, word):
+    number = int(num)
+    wordDigited = str(word)
+    wordRepeated = ""
+    for i in range (0, number, 1):
+        wordRepeated = wordRepeated + '<h1>' + word + '</h1>'
+    return wordRepeated
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
 
 #URL: http://127.0.0.1:5000
+
+# @app.route('/repeat/<number>/<word>')
+# def repeat(number,word):
+#     number = int(number)
+#     repeatWord = '' 
+#     for i in range (number):
+#         repeatWord +='<h1>'+ word + '</h1>'
+#     return repeatWord
